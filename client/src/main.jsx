ea66efd58bb9d4f18d5a3e1da85d4f2689185b4e
SadamRohith <sadamrohith456@gmail.com>
2025-12-13 11:43:14 -0600

    added configuration outputs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+// --- ADD THESE LINES ---
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json"; // Make sure this path is correct relative to main.jsx
+
+Amplify.configure(outputs);
+// -----------------------
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
