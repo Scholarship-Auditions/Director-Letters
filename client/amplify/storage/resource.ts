860a71844de496fdb56b360a56c8f30b6fc37f72
SadamRohith <sadamrohith456@gmail.com>
2026-01-03 16:45:56 -0600

    Add storage resource

--- client/amplify/storage/resource.ts
+++ client/amplify/storage/resource.ts
@@ -1,11 +1,10 @@
 import { defineStorage } from "@aws-amplify/backend";
-
 export const storage = defineStorage({
-  name: "letters",
+  name: "directorLetters",
   access: (allow) => ({
     "letters/*": [
       allow.guest.to(["read"]),
-      allow.authenticated.to(["read", "write"]),
+      allow.authenticated.to(["read", "write", "delete"]),
     ],
   }),
 });
