ad8d78e054fce7dc05ac023e194c04deea00e532
SadamRohith <sadamrohith456@gmail.com>
2026-01-03 16:14:03 -0600

    wrapped app under authenticator

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Authenticator } from "@aws-amplify/ui-react";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -12,6 +13,8 @@ Amplify.configure(outputs);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Authenticator.Provider>
+      <App />
+    </Authenticator.Provider>
   </StrictMode>
 );
